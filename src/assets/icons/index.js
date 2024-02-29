10d625f5e58db30415885b3c1a011816508ca8ac
chittaranjan24 <chittaranjanshit2@gmail.com>
2024-02-29 15:00:35 +0530

    Files are added

---
+++ src/assets/icons/index.js
@@ -0,0 +1,61 @@
+import css from './css.svg'
+import express from './express.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import mongodb from './mongodb.svg'
+import bootstrap from './bootstrap.png'
+import nextjs from './nextjs.svg'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import redux from './redux.svg'
+import sass from './sass.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+import php from './php.svg'
+import java from './java.svg'
+import gmail from './gmail.svg'
+import mysql from './mysql.svg'
+import apachetomcat from './apachetomcat.svg'
+import eclipse from './eclipse.svg'
+import vscode from './vscode.svg'
+import xampp from './xampp.svg'
+
+export {
+    apachetomcat,
+    eclipse,
+    vscode,
+    xampp,
+    php,
+    java,
+    gmail,
+    mysql,
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    mongodb,
+    bootstrap,
+    nextjs,
+    nodejs,
+    react,
+    redux,
+    sass,
+    tailwindcss,
+    typescript,
+    linkedin,
+    twitter,
+    arrow,
+    contact,
+    soundon,
+    soundoff
+}
---
+++ src/constants/index.js
@@ -0,0 +1,264 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    apachetomcat,
+    vscode,
+    eclipse,
+    xampp,
+    php,
+    java,
+    mysql,
+    gmail,
+    contact,
+    css,
+    bootstrap,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    nodejs,
+    react,
+    redux,
+    sass,
+    tailwindcss,
+    typescript,
+    twitter
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: bootstrap,
+        name: "BootStrap",
+        type: "Animation",
+    },
+    {
+        imageUrl: mysql,
+        name: "mySQL",
+        type: "Frontend",
+    },
+    {
+        imageUrl: php,
+        name: "Php",
+        type: "Frontend",
+    },
+    {
+        imageUrl: java,
+        name: "Java",
+        type: "Frontend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: vscode,
+        name: "VSCode",
+        type: "Version Control",
+    },
+    {
+        imageUrl: eclipse,
+        name: "Eclipse",
+        type: "Version Control",
+    },
+    {
+        imageUrl: xampp,
+        name: "Xampp",
+        type: "Version Control",
+    },
+    {
+        imageUrl: apachetomcat,
+        name: "Apache TomCat",
+        type: "Version Control",
+    },
+];
+
+export const experiences = [
+    {
+        title: "Jagran Lakecity University",
+        company_name: "MCA",
+        icon: "P.G",
+        iconBg: "#b9ebab",
+        date: "September 2022 - July 2024",
+        points: [
+            "Full Stack Developer",
+            "Cloud Computing",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "Tesla",
+        icon: 'U.G',
+        iconBg: "#fbc3bc",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Shopify",
+        icon: '12 th',
+        iconBg: "#b7e4c7",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Meta",
+        icon: "10 th",
+        iconBg: "#a2d2ff",
+        date: "Jan 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: 'tel:8436326242',
+    },
+    {
+        name: 'Email',
+        iconUrl: gmail,
+        link: 'mailto:chittaranjanshit2@gmail.com',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/chittaranjan-shit2',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/chittaranjan24',
+    },
+    {
+        name: 'Twitter',
+        iconUrl: twitter,
+        link: 'https://twitter.com/C_Shit24',
+    },
+];
+
+export const projects = [
+    // {
+    //     iconUrl: pricewise,
+    //     theme: 'btn-back-red',
+    //     name: 'Amazon Price Tracker',
+    //     description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+    //     link: 'https://github.com/adrianhajdin/pricewise',
+    // },
+    // {
+    //     iconUrl: threads,
+    //     theme: 'btn-back-green',
+    //     name: 'Full Stack Threads Clone',
+    //     description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+    //     link: 'https://github.com/adrianhajdin/threads',
+    // },
+    // {
+    //     iconUrl: car,
+    //     theme: 'btn-back-blue',
+    //     name: 'Car Finding App',
+    //     description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+    //     link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    // },
+    // {
+    //     iconUrl: snapgram,
+    //     theme: 'btn-back-pink',
+    //     name: 'Full Stack Instagram Clone',
+    //     description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+    //     link: 'https://github.com/adrianhajdin/social_media_app',
+    // },
+    // {
+    //     iconUrl: estate,
+    //     theme: 'btn-back-black',
+    //     name: 'Real-Estate Application',
+    //     description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+    //     link: 'https://github.com/adrianhajdin/projects_realestate',
+    // },
+    // {
+    //     iconUrl: summiz,
+    //     theme: 'btn-back-yellow',
+    //     name: 'AI Summarizer Application',
+    //     description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+    //     link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    // }
+];
